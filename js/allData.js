d7277cf3c6893e2647acabb7c903b505aa4f97e7
Brian <brian@melodimedia.co.uk>
2024-03-26 13:37:07 +0000

    update setting

--- js/allData.js
+++ js/allData.js
@@ -126,13 +126,30 @@ let data = {
         name:'COMMERCIAL',
         profilo:[
           {
-            id: 'about-videos1',
-            title: '【壞爸爸 Bad Daddy】',
-            url:'https://www.youtube.com/embed/Ffli-o0ocT0?si=zeLjtzglBG7qmga1'
+            id: 'COMMERCIAL1',
+            title: '【潮我展現變視度│Galaxy Z Fold2 5G樂趣潮展開】',
+            url:'https://www.youtube.com/watch?v=jeqkAGA_3n0',
+            author: '謝乾'
           },{
-            id: 'about-videos2',
-            title: 'TEST2',
-            url:'https://www.youtube.com/embed/d5PVbDDREW8?si=GIAf9AGI345WvGX5'
+            id: 'COMMERCIAL2',
+            title: '【718 Cayman Style Edition ​| Powered by Style】',
+            url:'https://youtu.be/5pSwEJw53Y8?si=pBYFulqXjsLLFrC3',
+            author: '謝乾'
+          },{
+            id: 'COMMERCIAL3',
+            title: 'CYTO® ❰WE ALL EVOLVE❱',
+            url:'https://youtu.be/KD5dGYzk9Bo?si=9yBOVzBiK7tFSr-w',
+            author: '肯尼'
+          },{
+            id: 'COMMERCIAL4',
+            title: '望月《我們還是一起》2023',
+            url:'https://www.youtube.com/watch?v=rVDtX8wmRgQ',
+            author: '小惡魔'
+          },{
+            id: 'COMMERCIAL5',
+            title: '第一屆金狼獎 入圍類別影片',
+            url:'https://www.youtube.com/watch?v=qyMIdPT4K4Q',
+            author: 'Loui'
           }
         ]
       },{
@@ -153,13 +170,145 @@ let data = {
         name:'MUSIC VIDOES',
         profilo:[
           {
-            id: 'about-videos1',
-            title: '【壞爸爸222 Bad Daddy】',
-            url:'https://www.youtube.com/embed/xZEdoCuxuA0?si=sr0f2fOFNgaTe3W7'
-          },{
-            id: 'about-videos2',
-            title: 'TEST22222ssss',
-            url:'https://www.youtube.com/embed/TH4iVLVqXAc?si=ayK-S5fg7CVtmEvI'
+            id: 'musicvideos1',
+            title: 'Starr Chen Feat. J. Sheon - 早上PAPAPA (Official Video)',
+            url:'https://www.youtube.com/watch?v=yOEpwDu7OB4',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos2',
+            title: 'aMEI張惠妹 feat.瘦子 E-SO [ Whatever 你說了算 ] Official Music Video',
+            url:'https://www.youtube.com/watch?v=QuNc8b7DxyY',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos3',
+            title: "J.Sheon - Don't Ask 別問很可怕 (Official Music Video)",
+            url:'https://www.youtube.com/watch?v=tRa3fOlnN7k',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos4',
+            title: "頑童MJ116【少年董 DON】Official Music Video",
+            url:'https://youtu.be/fIXTRr22_II?si=Gl0mOerB1ZkRWj3V',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos5',
+            title: "J.Sheon - Soul Away 囚 (Official Music Video)",
+            url:'https://youtu.be/XPQzKjisTbI?si=5nf461Nqtr4OBYrq',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos6',
+            title: "蘇運瑩《時候 Moment》Official Music Video",
+            url:'https://www.youtube.com/watch?v=F5XjLbMTo38',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos7',
+            title: '周湯豪NICKTHEREAL - "iGO ASIA REMIX" feat SIK-K JP THE WAVY official Music video',
+            url:'https://www.youtube.com/watch?v=FcCtOYGxf1Y',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos8',
+            title: 'YouTube Music Night with 周湯豪 NICKTHEREAL 十週年線上演唱會',
+            url:'https://www.youtube.com/watch?v=hQkQ2AzdNaY',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos9',
+            title: '瘦子E.SO【CHANGE】Official Music Video',
+            url:'https://youtu.be/HTRQ0n4yjfs?si=8hTSvMuQIE9jt8di',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos10',
+            title: '瘦子E.SO【她沒在看我 She Ain’t Watchin’】Official Music Video',
+            url:'https://youtu.be/smFGZKPS5yc?si=n9wlzWOOYr7ovyHk',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos11',
+            title: '瘦子E.SO【Follow You】Official Music Video',
+            url:'https://youtu.be/U1G4lKnYf4o?si=SqjZtQoEYG2Ervyc',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos12',
+            title: '瘦子E.SO【稱讚她的美 PRAISE】Official Music Video',
+            url:'https://youtu.be/YgNGl442hgg?si=QsEi1O32Scs5sj_R',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos13',
+            title: "盧廣仲 Crowd Lu【勵志論 C'MON】Official Music Video",
+            url:'https://www.youtube.com/watch?v=0Yi4WAR-9hM',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos14',
+            title: "HUR - 《Weirdo》 ft.G5SH MV",
+            url:'https://youtu.be/9eOQsGYs70c?si=FoHoPQN0-w2Roak0',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos15',
+            title: "瘦子E.SO【Way Up】Official Music Video",
+            url:'https://youtu.be/JfS8YZD5eSA?si=qsG-ra6E1AK0jkd_',
+            author: '謝乾'
+          }, {
+            id: 'musicvideos16',
+            title: "美麗本人【禿頭爸爸 Bald Head Daddy】官方音樂錄影帶 Official Music Video",
+            url:'https://youtu.be/xZEdoCuxuA0?si=CpENGd7yqXjqkjfa',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos17',
+            title: "瘦子E.SO【Something I Don't Need】Official Music Video",
+            url:'https://www.youtube.com/watch?v=8FS9MvbXoUc',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos18',
+            title: "美麗本人 李包比自傳feat. 簡道生 My Name is Dr. Beauty feat. DawsonC Official Music Video",
+            url:'https://www.youtube.com/watch?v=lhAvlkYlFc4',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos19',
+            title: "艾蜜莉AMILI - ''SUPA HERO'' feat. 蛋頭BG8LOCC (Official Music Video)",
+            url:'https://www.youtube.com/watch?v=wqAHERGPQnA',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos20',
+            title: "【Red Bull Orbit】超級英雄｜吳霏/ 比杰/ 小個 / 阿夫 (Prod. By KUMA)",
+            url:'https://www.youtube.com/watch?v=NHP0tQpPDNc',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos21',
+            title: "【Red Bull Orbit】古惑仔2023｜五木/ E1and / Quanzo / Novel Fergus (Prod. By W. Lin)",
+            url:'https://www.youtube.com/watch?v=5ZdYPweBqho',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos22',
+            title: "【Red Bull Orbit】台灣霹靂火 2023｜歐吉虎/ 王水源 / SheATH & Yappy (Prod. By KUMA)",
+            url:'https://www.youtube.com/watch?v=0_7dfs4qmrs',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos23',
+            title: "【Red Bull Orbit】GTA｜Henry / Zoe衛柔伊/ REX / O.Dkizzya (Prod. By Kuma)",
+            url:'https://www.youtube.com/watch?v=odkkoECQhOQ',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos24',
+            title: "【Red Bull Orbit】台南之子｜RPG / C Grass / 阿兄喬治/ YoungLee (Prod. By BurgerLin)",
+            url:'https://www.youtube.com/watch?v=z01EtQHeu2M',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos25',
+            title: "【Red Bull Orbit】NBA｜PIZZALI / Majin /小卡比/ High Loc (Prod. By Kuma)",
+            url:'https://www.youtube.com/watch?v=2anteD0kobk',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos26',
+            title: "李千娜 Nana Lee - 送予你（Official Music Video)",
+            url:'https://www.youtube.com/watch?v=GvrW9fUF0Mc',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos27',
+            title: "美麗本人 你是我的 Bae feat. 李千娜 You are my bae feat. Nana Lee Official Music Video",
+            url:'https://www.youtube.com/watch?v=hXhatbZ5j4k',
+            author: '小惡魔'
+          }, {
+            id: 'musicvideos28',
+            title: "草屯囝仔 Caotun Boyz - 救人喔 Help (Official Music Video)",
+            url:'https://youtu.be/HqhQR3adbok?si=CH0pu9G5xhJw0-iR',
+            author: 'ZC'
           }
         ]
       },
--- js/portfolio.js
+++ js/portfolio.js
@@ -12,54 +12,100 @@ $(function async(){
         while( i < profilo.length ){
             element.append(`
             <div class="swiper-slide">
-                <button type="button" data-id="${profilo[i].name}">${profilo[i].name}</button>
+                <button type="button" data-id="${profilo[i].name}" data-index="${i}">${profilo[i].name}</button>
             </div>`)
-            videosElement.append(`<div class="videos" data-set="${profilo[i].name}"><div class="swiper-wrapper"></div><div class="swiper-pagination"></div></div>`)
-            profilo[i].profilo.forEach((current,index)=>{
-                videosElement.find(`.videos[data-set="${profilo[i].name}"]`).find('.swiper-wrapper').append(`
-                <div class="swiper-slide">
-                    <div class="wrapper">
-                        <div class="blackScreen">
-                            <div class="button" data-profilotext="${index}" data-profilo="${i}">
-                                <img src="../assets/logo.png"/>
-                            </div>
-                        </div>
-                        <iframe id="youtube${index+1}" class="video" src="${current.url}?rel=0&loop=1&amp;autoplay=1&mute=1&enablejsapi=1&showinfo=0&playlist=${youtube_parser(current.url)}"  frameborder="0" allowfullscreen></iframe>
-                    </div>
-                </div>`
-                )
-            })
-
-            let swiper = new Swiper(`.videos[data-set="${profilo[i].name}"]`, {
-                slidesPerView:1,
-                spaceBetween: 0,
-                // loop: true,
-                // mousewheel: true,
-                // observer: true
-                direction: 'vertical',
-                // loop: true,
-                // mousewheel: true,
-                // observer: true,
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                    dynamicBullets: true,
-                    renderBullet: function (index, className) {
-                        return `
-                            <div class="pagination-subtitle ${className}" >
-                                <span class="text"> ${profilo[i].profilo[index].title}</span>
-                            </div>
-                        `;
-                    }
-                },
-            });
             i++
         }
-    
     }
+    let storeIndex = []
+    const initVideos = (i)=>{
+        if(storeIndex.indexOf(i) !== -1) return ;
+        storeIndex.push(i)
+        videosElement.append(`<div class="videos" data-set="${profilo[i].name}"><div class="swiper-wrapper"></div><div class="swiper-pagination"></div></div>`)
+        profilo[i].profilo.forEach((current,index)=>{
+            videosElement.find(`.videos[data-set="${profilo[i].name}"]`).find('.swiper-wrapper').append(`
+            <div class="swiper-slide">
+                <div class="wrapper wrapper${index}">
+                    <div class="blackScreen">
+                        <div class="button" data-profilotext="${index}" data-profilo="${i}">
+                            <img src="../assets/logo.png"/>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>`
+            )
+        })
+
+        let swiper = new Swiper(`.videos[data-set="${profilo[i].name}"]`, {
+            slidesPerView:1,
+            spaceBetween: 0,
+            // loop: true,
+            // mousewheel: true,
+            // observer: true
+            direction: 'vertical',
+            // loop: true,
+            // mousewheel: true,
+            // observer: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                renderBullet: function (index, className) {
+                    return `
+                        <div class="pagination-subtitle ${className}" >
+                            <span class="text"> ${profilo[i].profilo[index].title}</span>
+                        </div>
+                    `;
+                }
+            },
+        });
+        let storeForIframe = [];
+        if(storeForIframe.length < 1){
+            updateIframe()
+        }
+
+        function updateIframe() {
+            let currentIndex = swiper.activeIndex;
+            if(storeForIframe.indexOf(currentIndex) !== -1) return;
+            storeForIframe.push(currentIndex);
+            let currentUrl = profilo[i].profilo[currentIndex].url;
+            console.log(swiper.activeIndex,profilo[i],'testInit')
+            videosElement.find(`.videos[data-set="${profilo[i].name}"]`).find('.swiper-wrapper').find(`.wrapper${currentIndex}`).append(`
+                <iframe id="youtube${currentIndex+1}" class="video" src="https://www.youtube.com/embed/${youtube_parser(currentUrl)}?rel=0&loop=1&amp;autoplay=1&mute=1&enablejsapi=1&showinfo=0&playlist=${youtube_parser(currentUrl)}"  frameborder="0" allowfullscreen></iframe>
+            `)
+
+        }
+       
+        swiper.on('slideChange', function () {
+            // console.log(swiper.activeIndex,profilo[i],'test')
+            updateIframe();
+            // let currentIndex = swiper.activeIndex;
+            // let currentUrl = profilo[i].profilo[currentIndex].url;
+            // videosElement.find(`.videos[data-set="${profilo[i].name}"]`).find('.swiper-wrapper').find(`.wrapper${currentIndex}`).append(`
+            //     <iframe id="youtube${currentIndex+1}" class="video" src="https://www.youtube.com/embed/${youtube_parser(currentUrl)}?rel=0&loop=1&amp;autoplay=1&mute=1&enablejsapi=1&showinfo=0&playlist=${youtube_parser(currentUrl)}"  frameborder="0" allowfullscreen></iframe>
+            // `)
+        });
+        $('.videos .swiper-wrapper').on('click', '.button', function(){
+            stopAllYouTubeVideos();
+            const mainSet = $(this).data('profilo');
+            const subSet  = $(this).data('profilotext')
+            const getURL = profilo[mainSet].profilo[subSet].url;
+            $('.dialog').show()
+            $('#dialog-wrapper').empty();
+            $('#dialog-wrapper').append(`
+                <iframe id="youtube${mainSet+1}" class="dialogvideo" src="https://www.youtube.com/embed/${youtube_parser(getURL)}?rel=0&loop=1&amp;autoplay=1&mute=1&enablejsapi=1&showinfo=0&playlist=${youtube_parser(getURL)}"  frameborder="0"></iframe>
+            `)
+            // do something here
+        });
+    }
+
+
 $('.profilo-swiper').on('click','button',function(e){
     console.log('swiper clicked '+ e.target.dataset.id);
     let currentName = e.target.dataset.id;
+    initVideos(e.target.dataset.index)
+    
     let videosElement = $('.profilo')
     $('.profilo-swiper').find('button').removeClass('active');
     e.target.className = 'active'
@@ -72,19 +118,7 @@ var stopAllYouTubeVideos = () => {
         iframe.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
     });
 }
-$('.videos .swiper-wrapper').on('click', '.button', function(){
-    stopAllYouTubeVideos();
-    const mainSet = $(this).data('profilo');
-    const subSet  = $(this).data('profilotext')
-    const getURL = profilo[mainSet].profilo[subSet].url;
 
-    $('.dialog').show()
-    $('#dialog-wrapper').empty();
-    $('#dialog-wrapper').append(`
-        <iframe id="youtube-dialog${mainSet+1}" class="dialogvideo" src="${getURL}}?rel=0&loop=1&autoplay=1&mute=0&enablejsapi=1&showinfo=0"  allow='autoplay' allowfullscreen frameborder="0" ></iframe>
-    `)
-    // do something here
-});
 
     const initSwiper = () => {
         let swiper = new Swiper('.profilo-swiper', {
