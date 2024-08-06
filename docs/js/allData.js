2b29eadb6f4cd0f571a30e7d95cc72e94e605f1e
Brian <brian@melodimedia.co.uk>
2024-08-06 17:11:40 +0100

    更新資料 20240806

--- docs/js/allData.js
+++ docs/js/allData.js
@@ -3,6 +3,9 @@ let data = {
     homeVideos:[{
         id: 'home-videos1',
         url:'https://www.youtube.com/embed/Ffli-o0ocT0?si=zeLjtzglBG7qmga1'
+    },{
+      id: 'home-videos2',
+      url:'https://www.youtube.com/embed/Asqih2ftbdc?si=CzKdVsAOvd41FNgE'
     }],
     // about Page
     aboutVideos:[{
